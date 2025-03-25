569cffe4327e8b765e9f3dc3a73eee366b7ab3df
mbuvic <charlesmbuvipeter@gmail.com>
2025-03-25 17:30:19 +0300

    form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
+Route::get('/register', [UserController::class, 'index']);
